da2c3af42621e382d180682cf14f56f81322372a
Sophia Ribeiro <sophiaelmayarah@gmail.com>
2025-11-04 23:35:32 -0300

    concatenação de string

---
+++ Javascript/concatenação e template string/script.js
@@ -0,0 +1,13 @@
+// concatenação de strings são com a formaatação de tesxto em python.
+
+const saudacoes = "Olá ";
+const nome = "Sophia";
+
+//console.log (saudacoes + nome + "!");
+
+// Template String (Template Literal)
+// Usando crase ` ` ao invés de aspas '' ou "". jeito mais moderno de concatenar strings.
+// Permite a interpolação de variáveis e expressões dentro da string usando ${}.
+//sempre usar crase para template string.
+
+console.log (`${saudacoes} ${nome} seja bem-vinda ao curso de JavaScript!`);
