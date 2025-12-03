4fceafd36993faff64f572ca029275a9c7d5ae0d
Sophia Ribeiro <sophiaelmayarah@gmail.com>
2025-12-02 23:29:32 -0300

    loops

---
+++ Javascript/loops/script.js
@@ -0,0 +1,46 @@
+// while(condição){
+    //Executa ENQUANTO a condição for verdadeira
+//}
+
+//let numero = 1
+//while(numero <= 4){
+//    console.log('Executando o while ' + numero);
+//    numero = numero + 1
+//    numero++
+//}
+
+//do.. while  do = faça
+//while = enquanto
+// primeiro faz o teste com o do e depois verifica a condição com o while
+/*
+let numero = 0
+do{
+    console.log('Executando o do while testeclae ' + numero);
+    numero++
+}while(numero <= 8)
+*/
+
+
+// for = para
+// for (condição de início; condição de fim; incremento){
+    //Executa o bloco de código
+//} 
+
+/*for(let numero = 1; numero <= 4; numero++){
+    console.log('Executando o for ' + numero);
+}*/
+
+//length = le quantos itens tem dentro do array
+// indice in tambem le os itens do array
+
+/*let postagens = [
+    {titulo: 'Aprendendo JavaScript', autor: 'Diego'},
+    {titulo: 'JavaScript e Java', autor: 'Ana'},
+    {titulo: 'JavaScript para iniciantes', autor: 'Jorge'},
+    {titulo: 'JavaScript Avançado', autor: 'Maria'}
+]
+
+for(i in postagens){
+    console.log(postagens[i].titulo + ' por ' + postagens[i].autor);
+}
+*/
