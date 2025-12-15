6df75dbb91b4d038df94578022e863cacfddbae6
Sophia Ribeiro <sophiaelmayarah@gmail.com>
2025-12-15 18:43:08 -0300

    atribuição

---
+++ Javascript/Operadores/atribuição.js
@@ -0,0 +1,16 @@
+let salario = 500
+let bonus = 100
+
+salario += bonus // salario = salario + bonus
+console.log(salario) // 600
+
+salario -= bonus // salario = salario - bonus
+console.log(salario) // 500  
+
+salario *= 2 // salario = salario * 2
+console.log(salario) // 1000
+
+salario /= 4 // salario = salario / 4
+console.log(salario) // 250
+
+//é o mesmo que salario = salario - desconto
